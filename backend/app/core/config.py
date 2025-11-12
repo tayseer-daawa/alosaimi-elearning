2afe0c8a639223a71396ac7c391212060a8f270a
Merzouk-Ilyes <merzouk.ilyase@gmail.com>
2025-11-12 10:04:05 +0100

    fix(backend/student): fixed  CORS config, docker-compose.override & .github/workflows/generate-client

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -35,6 +35,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_ADMIN_HOST: str = "http://localhost:5173"
+    FRONTEND_STUDENT_HOST: str = "http://localhost:5174"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
@@ -45,7 +46,8 @@ class Settings(BaseSettings):
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-            self.FRONTEND_ADMIN_HOST
+            self.FRONTEND_ADMIN_HOST,
+            self.FRONTEND_STUDENT_HOST,
         ]
 
     PROJECT_NAME: str
