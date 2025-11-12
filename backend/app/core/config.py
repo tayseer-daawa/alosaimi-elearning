e31d1cbe21355ca3d81bc971c8679d437ea5ba54
Merzouk-Ilyes <merzouk.ilyase@gmail.com>
2025-11-12 10:50:11 +0100

    fix(frontend/student): forgot comma in backend/app/core

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -46,7 +46,7 @@ class Settings(BaseSettings):
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-            self.FRONTEND_ADMIN_HOST
+            self.FRONTEND_ADMIN_HOST,
             self.FRONTEND_STUDENT_HOST
         ]
 
