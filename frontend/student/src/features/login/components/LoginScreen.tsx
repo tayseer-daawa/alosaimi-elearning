16796f6a23753d88aab3529cdb6b9ce27602b34f
Mehdi Rahal <mehdi@lablabee.com>
2025-12-17 10:19:47 +0100

    feat(frontend/student): make login UI responsive

--- frontend/student/src/features/login/components/LoginScreen.tsx
+++ frontend/student/src/features/login/components/LoginScreen.tsx
@@ -1,5 +1,49 @@
-import { Box, Button, Container, Flex, HStack, Image, Input, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  InputProps,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { useLoginWizard } from '../hooks/useLoginWizard';
+
+const CustomVStackInput = ({
+  label,
+  state,
+  stateSetter,
+  ...props //input props
+}: {
+  label: string;
+  state: string;
+  stateSetter: (value: string) => void;
+} & Omit<InputProps, 'value' | 'onChange'>) => {
+  return (
+    <VStack w={{ base: '90%', md: '45%', lg: '35%' }} gap={{ base: 3, md: 4 }} align="stretch">
+      <Text fontSize={{ base: 'sm', md: '2xl', lg: '3xl' }} color="text.default">
+        {label}
+      </Text>
+      <Input
+        size={{ base: 'md', lg: 'xl' }}
+        fontSize={{ base: 'xl', md: '2xl', lg: '4xl' }}
+        fontWeight={600}
+        variant="flushed"
+        value={state}
+        onChange={(e) => stateSetter(e.target.value)}
+        placeholder=""
+        borderColor="brand.primary"
+        borderBottomWidth={{ base: '2px', md: '3px', lg: '4px' }}
+        _focus={{ borderColor: 'brand.primary', borderBottomWidth: '3px' }}
+        height={{ base: 16, md: 24, lg: 24 }}
+        {...props}
+      />
+    </VStack>
+  );
+};
 
 export default function LoginScreen() {
   const {
@@ -11,8 +55,8 @@ export default function LoginScreen() {
     setFullName,
     email,
     setEmail,
-    wantsLearning,
-    setWantsLearning,
+    wantsNotifications,
+    setWantsNotifications,
     password,
     setPassword,
     confirmPassword,
@@ -21,82 +65,84 @@ export default function LoginScreen() {
   } = useLoginWizard();
 
   return (
-    <Box dir="rtl" minH="100vh" py={{ base: 10, md: 14 }}>
-      <Container maxW="sm">
-        <Flex minH="calc(100vh - 80px)" direction="column" gap={10}>
-          <VStack gap={2} pt={{ base: 4, md: 8 }}>
-            <Text fontSize={{ base: '2xl', md: '3xl' }} fontWeight={700} textAlign="center">
+    <Box dir="rtl" minH="100vh" py={{ base: 10, md: 16, lg: 20 }}>
+      <Container>
+        <Flex minH="calc(100vh - 80px)" direction="column" gap={{ base: 10, md: 12, lg: 14 }}>
+          <VStack gap={{ base: 2, md: 3 }} pt={{ base: 4, md: 10, lg: 12 }}>
+            <Text
+              fontSize={{ base: '2xl', md: '4xl', lg: '5xl' }}
+              fontWeight={700}
+              textAlign="center"
+              lineHeight={{ base: 'short', md: 'shorter' }}
+            >
               {title}
             </Text>
           </VStack>
 
           {step === 'welcome' ? (
-            <VStack gap={8} flex="1" justify="center">
-              <Image
-                src="/assets/mecque.svg"
-                alt="Mecque illustration"
-                maxW={{ base: '80%', md: '70%' }}
-                mx="auto"
-              />
-            </VStack>
+            <Image
+              src="/assets/mecque.svg"
+              alt="Mecque illustration"
+              width={{ base: '80%', md: '65%', lg: '60%' }}
+              height="100%"
+              mx="auto"
+              my="auto"
+            />
           ) : (
-            <VStack gap={8} flex="1" justify="center">
+            // </VStack>
+            <VStack gap={{ base: 8, md: 10, lg: 12 }} flex="1" justify="center">
               {step === 'name' && (
-                <VStack w="100%" gap={3} align="stretch">
-                  <Text fontSize="sm" color="text.default">
-                    الاسم الكامل
-                  </Text>
-                  <Input
-                    variant="flushed"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                    placeholder=""
-                    autoComplete="name"
-                  />
-                </VStack>
+                <CustomVStackInput
+                  label="الاسم الكامل"
+                  state={fullName}
+                  stateSetter={setFullName}
+                  autoComplete="name"
+                />
               )}
 
               {step === 'email' && (
-                <VStack w="100%" gap={3} align="stretch">
-                  <Text fontSize="sm" color="text.default">
-                    البريد الإلكتروني
-                  </Text>
-                  <Input
-                    variant="flushed"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder=""
-                    type="email"
-                    autoComplete="email"
-                  />
-                </VStack>
+                <CustomVStackInput
+                  label="البريد الإلكتروني"
+                  state={email}
+                  stateSetter={setEmail}
+                  autoComplete="email"
+                  type="email"
+                />
               )}
 
               {step === 'goal' && (
-                <VStack w="100%" gap={5} align="stretch">
-                  <Text fontSize="sm" color="text.default">
+                <VStack
+                  w={{ base: '90%', md: '45%', lg: '35%' }}
+                  gap={{ base: 3, md: 4 }}
+                  align="stretch"
+                >
+                  <Text fontSize={{ base: 'sm', md: 'md', lg: 'lg' }} color="text.default">
                     هل تريد تلقي بعض الاشعارات على البريد الالكتروني؟
                   </Text>
-                  <HStack gap={3} justify="center">
+                  <HStack gap={{ base: 3, md: 4 }} justify="space-between">
                     <Button
-                      variant={wantsLearning === true ? 'solid' : 'outline'}
-                      onClick={() => setWantsLearning(true)}
+                      size={{ base: 'md', md: 'lg' }}
+                      variant={wantsNotifications === true ? 'solid' : 'outline'}
+                      onClick={() => setWantsNotifications(true)}
                       borderColor="brand.primary"
-                      color={wantsLearning === true ? 'white' : 'brand.primary'}
-                      bg={wantsLearning === true ? 'brand.primary' : 'transparent'}
-                      _hover={{ bg: wantsLearning === true ? 'brand.primary' : 'transparent' }}
-                      w="40%"
+                      color={wantsNotifications === true ? 'white' : 'brand.primary'}
+                      bg={wantsNotifications === true ? 'brand.primary' : 'transparent'}
+                      _hover={{ bg: wantsNotifications === true ? 'brand.primary' : 'transparent' }}
+                      w={{ base: '40%', md: '45%' }}
                     >
                       نعم
                     </Button>
                     <Button
-                      variant={wantsLearning === false ? 'solid' : 'outline'}
-                      onClick={() => setWantsLearning(false)}
+                      size={{ base: 'md', md: 'lg' }}
+                      variant={wantsNotifications === false ? 'solid' : 'outline'}
+                      onClick={() => setWantsNotifications(false)}
                       borderColor="brand.primary"
-                      color={wantsLearning === false ? 'white' : 'brand.primary'}
-                      bg={wantsLearning === false ? 'brand.primary' : 'transparent'}
-                      _hover={{ bg: wantsLearning === false ? 'brand.primary' : 'transparent' }}
-                      w="40%"
+                      color={wantsNotifications === false ? 'white' : 'brand.primary'}
+                      bg={wantsNotifications === false ? 'brand.primary' : 'transparent'}
+                      _hover={{
+                        bg: wantsNotifications === false ? 'brand.primary' : 'transparent',
+                      }}
+                      w={{ base: '40%', md: '45%' }}
                     >
                       لا
                     </Button>
@@ -105,44 +151,40 @@ export default function LoginScreen() {
               )}
 
               {step === 'password' && (
-                <VStack w="100%" gap={6} align="stretch">
-                  <VStack gap={3} align="stretch">
-                    <Text fontSize="sm" color="text.default">
-                      كلمة السر
-                    </Text>
-                    <Input
-                      variant="flushed"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      type="password"
-                      autoComplete="new-password"
-                    />
-                  </VStack>
-                  <VStack gap={3} align="stretch">
-                    <Text fontSize="sm" color="text.default">
-                      تأكيد كلمة السر
-                    </Text>
-                    <Input
-                      variant="flushed"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                      type="password"
-                      autoComplete="new-password"
-                    />
-                  </VStack>
+                <VStack w="100%" gap={{ base: 6, md: 8 }} align="center">
+                  <CustomVStackInput
+                    label="كلمة السر"
+                    state={password}
+                    stateSetter={setPassword}
+                    type="password"
+                    autoComplete="new-password"
+                  />
+                  <CustomVStackInput
+                    label="تأكيد كلمة السر"
+                    state={confirmPassword}
+                    stateSetter={setConfirmPassword}
+                    type="password"
+                    autoComplete="new-password"
+                  />
                 </VStack>
               )}
 
               {error ? (
-                <Text fontSize="sm" color="red.500" textAlign="center">
+                <Text fontSize={{ base: 'sm', md: 'md' }} color="red.500" textAlign="center">
                   {error}
                 </Text>
               ) : null}
             </VStack>
           )}
 
-          <Box pb={{ base: 6, md: 8 }}>
+          <Box
+            pb={{ base: 6, md: 10, lg: 12 }}
+            width={{ base: '80%', md: '45%', lg: '30%' }}
+            mx="auto"
+          >
             <Button
+              size={{ base: 'md', md: '2xl' }}
+              fontSize={{ base: 'auto', md: '2xl', lg: '3xl' }}
               w="100%"
               bg="brand.primary"
               color="white"
