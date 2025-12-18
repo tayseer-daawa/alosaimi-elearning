29b4113687d14a2a5fce464f3e08fb3d7bfa44c1
Mehdi Rahal <mehdi@lablabee.com>
2025-12-18 09:57:22 +0100

    fix(frontend/student): rename login to signup

--- frontend/student/src/routes/_layout/login.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import LoginScreen from '@/features/login/components/LoginScreen';
-export const Route = createFileRoute('/_layout/login')({
-  component: LoginScreen,
-});
