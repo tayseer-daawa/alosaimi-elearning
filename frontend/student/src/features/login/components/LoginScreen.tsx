920b155c83ff458bc81d5c2904d85611a02d8b6b
Mehdi Rahal <mehdi@lablabee.com>
2026-04-01 12:59:26 +0100

    feat(frontend): implement AuthLayout component and refactor screens to use it

--- frontend/student/src/features/login/components/LoginScreen.tsx
+++ frontend/student/src/features/login/components/LoginScreen.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react"
 import { useNavigate } from "@tanstack/react-router"
 import { CustomField } from "@/shared/components/CustomField"
 import { useLoginWizard } from "../hooks/useLoginWizard"
+import AuthLayout from "@/shared/components/AuthLayout"
 
 export default function LoginScreen() {
   const {
@@ -24,92 +25,72 @@ export default function LoginScreen() {
   }
 
   return (
-    <Box
-      dir="rtl"
-      h="100vh"
-      p={{ base: "calc(5rem + 3vh) 2.5rem calc(3rem + 1vh)", md: 16, lg: 20 }} //Magic numbers are calculated from figma to fit different mobile heights
-    >
-      <Box
-        p={0}
-        h="100%"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Flex
-          h="100%"
-          direction="column"
-          gap="auto"
-          justifyContent={"space-between"}
-          w={{ base: "100%", md: "80%", lg: "60%" }}
+    <AuthLayout>
+      <VStack gap={{ base: 2, md: 3 }} pt={{ base: 4, md: 10, lg: 12 }}>
+        <Text
+          fontSize={{ base: "3xl", md: "5xl", lg: "5xl" }}
+          fontWeight={400}
+          textAlign="center"
+          lineHeight={{ base: "short", md: "shorter" }}
         >
-          <VStack gap={{ base: 2, md: 3 }} pt={{ base: 4, md: 10, lg: 12 }}>
-            <Text
-              fontSize={{ base: "3xl", md: "5xl", lg: "5xl" }}
-              fontWeight={400}
-              textAlign="center"
-              lineHeight={{ base: "short", md: "shorter" }}
-            >
-              {title}
-            </Text>
-          </VStack>
-          <VStack
-            gap={{ base: 8, md: 10, lg: 12 }}
-            flex="1"
-            justify="center"
-            w={{ base: "100%", md: "75%", lg: "65%" }}
-            alignSelf="center"
-          >
-            <CustomField
-              label="البريد الإلكتروني"
-              state={email}
-              stateSetter={setEmail}
-              autoComplete="email"
-              type="email"
-              handleKeyDownEnter={handleKeyDownEnter}
-              error={error?.email}
-            />
-            <Box w="100%">
-              <CustomField
-                label="كلمة السر"
-                state={password}
-                stateSetter={setPassword}
-                type="password"
-                autoComplete="new-password"
-                handleKeyDownEnter={handleKeyDownEnter}
-                error={error?.password}
-              />
-              <Button
-                alignSelf="self-start"
-                fontSize={{ base: "sm", md: "xl", lg: "xl" }}
-                marginTop={{
-                  base: 0,
-                  md: 4,
-                  lg: 6,
-                }}
-                fontWeight="500"
-                color="text.default"
-                variant="ghost"
-                p={0}
-                textDecoration="underline"
-                onClick={() => navigate({ to: "/forget-password" })}
-              >
-                نسيت كلمة السر؟
-              </Button>
-            </Box>
-          </VStack>
+          {title}
+        </Text>
+      </VStack>
+      <VStack
+        gap={{ base: 8, md: 10, lg: 12 }}
+        flex="1"
+        justify="center"
+        w={{ base: "100%", md: "75%", lg: "65%" }}
+        alignSelf="center"
+      >
+        <CustomField
+          label="البريد الإلكتروني"
+          state={email}
+          stateSetter={setEmail}
+          autoComplete="email"
+          type="email"
+          handleKeyDownEnter={handleKeyDownEnter}
+          error={error?.email}
+        />
+        <Box w="100%">
+          <CustomField
+            label="كلمة السر"
+            state={password}
+            stateSetter={setPassword}
+            type="password"
+            autoComplete="new-password"
+            handleKeyDownEnter={handleKeyDownEnter}
+            error={error?.password}
+          />
           <Button
-            size={{ base: "md", md: "md" }}
-            w={{ base: "80%", md: "60%", lg: "50%" }}
-            alignSelf="center"
-            onClick={next}
-            loading={isSubmitting}
-            disabled={isSubmitting}
+            alignSelf="self-start"
+            fontSize={{ base: "sm", md: "xl", lg: "xl" }}
+            marginTop={{
+              base: 0,
+              md: 4,
+              lg: 6,
+            }}
+            fontWeight="500"
+            color="text.default"
+            variant="ghost"
+            p={0}
+            textDecoration="underline"
+            onClick={() => navigate({ to: "/forget-password" })}
           >
-            مواصلة
+            نسيت كلمة السر؟
           </Button>
-        </Flex>
-      </Box>
-    </Box>
+        </Box>
+      </VStack>
+      <Button
+        size={{ base: "md", md: "md" }}
+        w={{ base: "80%", md: "60%", lg: "50%" }}
+        alignSelf="center"
+        onClick={next}
+        loading={isSubmitting}
+        disabled={isSubmitting}
+      >
+        مواصلة
+      </Button>
+    </AuthLayout>
   )
 }
