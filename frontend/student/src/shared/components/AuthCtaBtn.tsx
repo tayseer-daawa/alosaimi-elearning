fbcd7d72218acb12e34e2fb5bce86f40bb2cfb82
Mehdi Rahal <mehdi@lablabee.com>
2026-04-11 09:20:30 +0100

    refactor(frontend): rename AuthBtnCta to AuthCtaBtn and update mobile button size to sm

--- frontend/student/src/shared/components/AuthCtaBtn.tsx
+++ frontend/student/src/shared/components/AuthCtaBtn.tsx
@@ -1,10 +1,10 @@
 import type { ButtonProps } from "@chakra-ui/react"
 import { Button } from "@chakra-ui/react"
 
-export default function AuthBtnCta({ children, ...props }: ButtonProps) {
+export default function AuthCtaBtn({ children, ...props }: ButtonProps) {
   return (
     <Button
-      size={{ base: "md", md: "md" }}
+      size={{ base: "sm", md: "md" }}
       w={{ base: "80%", md: "60%", lg: "50%" }}
       alignSelf="center"
       {...props}
